d5b2f976ba9d84d5685ae3003983d62e7180b0fd
Minnnning <6alswjd6@gmail.com>
2025-10-30 09:09:44 +0900

    main.py 변경

--- main.py
+++ main.py
@@ -11,38 +11,17 @@ from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime
 import logging
 from prometheus_fastapi_instrumentator import Instrumentator
-from prometheus_client import Counter, Gauge, Histogram 
-import time
+
 
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # FastAPI 앱 초기화
-app = FastAPI(title="My API", version="1.0.0")
+app = FastAPI(title="My API", version="1.2.0")
 
 # 앱에 메트릭 수집기 적용 및 /metrics 엔드포인트 노출
 Instrumentator().instrument(app).expose(app)
-
-# Counter: 오직 증가만 하는 값 (예: 생성된 총 아이템 수, 발생한 에러 수)
-ITEMS_CREATED_COUNTER = Counter(
-    "items_created_total", 
-    "Total number of items created",
-    ["item_type"]  # 'item_type'별로 구분해서 집계 (라벨)
-)
-
-# Gauge: 오르내릴 수 있는 값 (예: 현재 활성 사용자 수, 현재 DB 커넥션 수)
-ACTIVE_CONNECTIONS_GAUGE = Gauge(
-    "active_database_connections",
-    "Current number of active database connections"
-)
-
-# Histogram: 분포(시간, 크기)를 측정 (예: 특정 함수의 처리 시간)
-DB_QUERY_DURATION_SECONDS = Histogram(
-    "db_query_duration_seconds",
-    "Histogram for database query durations",
-    ["query_type"] # 'query_type'별로 구분
-)
 
 # CORS 설정
 app.add_middleware(
@@ -143,16 +122,12 @@ def get_db():
     if SessionLocal is None:
         raise HTTPException(status_code=500, detail="Database not initialized")
     
-    #Gauge 사용 (커넥션 생성 시 +1)
-    ACTIVE_CONNECTIONS_GAUGE.inc() 
     
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        #커넥션 반환 시 -1)
-        ACTIVE_CONNECTIONS_GAUGE.dec()
 
 @app.on_event("startup")
 async def startup_event():
@@ -165,7 +140,7 @@ async def root():
     """헬스 체크 엔드포인트"""
     return {
         "message": "API is running",
-        "version": "1.1.0",
+        "version": "1.2.0",
         "status": "healthy"
     }
 
@@ -204,21 +179,11 @@ async def get_item(item_id: int, db: Session = Depends(get_db)):
 @app.post("/items", response_model=ItemResponse, status_code=201)
 async def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     """새 아이템 생성"""
-    # Counter 사용(아이템 생성 시 +1)
-    # normal이라는 라벨 값으로 카운터 1 증가
-    ITEMS_CREATED_COUNTER.labels(item_type="normal").inc()
-    
-    # Histogram 사용(DB 작업 시간 측정)
-    start_time = time.time() # 시작 시간 기록
     
     db_item = Item(name=item.name, description=item.description)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
-    
-    duration = time.time() - start_time # 소요 시간 계산
-    # create_item_query 라벨로 소요 시간(duration)을 기록
-    DB_QUERY_DURATION_SECONDS.labels(query_type="create_item_query").observe(duration)
     
     return db_item
 
